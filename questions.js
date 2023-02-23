69e5405d549a7f128a25ebc5f666f16e0554f906
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-02-22 20:36:38 -0600

    Adding quiz questions array

---
+++ questions.js
@@ -0,0 +1,52 @@
+const quiz = [
+{
+    question: "What is JavaScript?",
+    anwers: ["A programming language for the web", "A type of coffee", "A brand of shoes", "A type of car"],  
+    correct: "A programming language for the web"
+},
+{
+    question: "What is the DOM in JavaScript?",
+    answers: ["A tree-like structure that represents the HTML of a web page", "A type of programming language", "A way to store data in a database", "A type of coffee"], 
+    correct: "A tree-like structure that represents the HTML of a web page"
+},
+{
+    question: "What is a closure in JavaScript?", 
+    answers: ["A function that returns another function", "A way to close a web page", "A type of variable", "A type of coffee"],
+    correct: "A function that returns another function"
+},
+{
+    question: "What is the difference between let and var in JavaScript?",
+    anwers:[ "let is block-scoped, while var is function-scoped", "let is function-scoped, while var is block-scoped", "let is a reserved word in JavaScript, while var is not", "let is a newer keyword in JavaScript, while var is an older one"],
+    correct: "let is block-scoped, while var is function-scoped"
+},
+{
+    question: "What is an event listener in JavaScript?",
+    answers:["A function that listens for a specific event to occur and then executes a callback function", "A way to add styles to a web page", "A type of loop in JavaScript", "A type of coffee"],
+    correct: "A function that listens for a specific event to occur and then executes a callback function"
+},
+{
+    question: "What is the purpose of the keyword 'this' in JavaScript?",
+    anwers: ["To refer to the current object", "To refer to the global object", "To refer to a different object", "To refer to a variable"],
+    correct: "To refer to the current object"
+},
+{
+    question: "What is the difference between synchronous and asynchronous JavaScript?",
+    answers: ["Synchronous code blocks the execution of other code until it is complete, while asynchronous code allows other code to run while it is waiting for a response", "Synchronous code allows other code to run while it is waiting for a response, while asynchronous code blocks the execution of other code until it is complete", "Synchronous code and asynchronous code are the same thing", "Synchronous code is faster than asynchronous code"],
+    correct: "Synchronous code blocks the execution of other code until it is complete, while asynchronous code allows other code to run while it is waiting for a response"
+},
+{
+    question:"What is the purpose of the 'use strict' directive in JavaScript?", 
+    anwers: ["To enable strict mode, which enforces stricter rules for writing JavaScript code", "To disable strict mode, which allows for looser rules for writing JavaScript code", "To enable debugging mode in JavaScript", "To enable automatic semicolon insertion in JavaScript"],
+    correct: "To enable strict mode, which enforces stricter rules for writing JavaScript code"
+},
+{
+    question: "What is the difference between null and undefined in JavaScript?", 
+    answers: ["null represents a deliberate non-value, while undefined represents an unintentional absence of a value", "undefined represents a deliberate non-value, while null represents an unintentional absence of a value", "null and undefined are the same thing", "null is a reserved word in JavaScript, while undefined is not"],
+    correct: "null represents a deliberate non-value, while undefined represents an unintentional absence of a value"
+},
+{
+    question: "What is the purpose of the 'typeof' operator in JavaScript?", 
+    answers: ["To determine the type of a value", "To convert a value to a different type", "To create a new variable", "To declare a function"],
+    correct: "To determine the type of a value"
+}
+];
