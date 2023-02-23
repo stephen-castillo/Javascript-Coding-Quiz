5d1eb769f62ed9b8b0cf8991af9270b73c2a6f42
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-02-22 21:38:29 -0600

    Screenshot

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Javascript-Coding-Quiz
+
+
+
+![Screenshot](https://user-images.githubusercontent.com/122939431/220816197-613d00d0-700b-4593-89e0-ce296556589b.JPG)
--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-let correct = 0;
-let wrong = 0;
+/* Set variables for count of correct questions and wrong questions answered */
+var correct = 0;
+var wrong = 0;
+var time = 240;
+var timer = 0;
+
+
+/* Need timer function */
+
+
+/* Check if timer is still active AND if wrong answer still = 0 */
+
+
+/* for loop to go through questions and answers and replace html conent */
+
+    /* Event handler for user to submit answer to quiz question */
+
+
+    /* Check answer submitted against correct answer 
+    If correct update correct count and continue
+    else break and return wrong answer message*/
+
+
+
+
+
+/* Need event handler for quiz start */
