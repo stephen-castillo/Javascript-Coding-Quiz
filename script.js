dedd93f6e25ee809260dfda8e786776c000042ba
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-02-26 22:12:51 -0600

    Added timer function and button event handlers

--- script.js
+++ script.js
@@ -2,11 +2,38 @@
 var correct = 0;
 var wrong = 0;
 var time = 240;
-var timer = 0;
+var clock = document.getElementById('clock');
+var start = document.getElementById('start');
+var cancel = document.getElementById('cancel');
+var gameclock;
 
+console.log(clock);
+console.log(start);
+
+
+/* Count down funciton */
+/* setInterval(function(){
+    time--;
+    console.log(time);
+    clock.innerText = 'Time left: ' + time + ' seconds';
+}, 1000); */
 
 /* Need timer function */
+function timer(event){
+    event.preventDefault();
+    gameclock = setInterval(function(){
+        time--;
+        console.log(time);
+        clock.innerText = 'Time left: ' + time + ' seconds';
+    }, 1000);
+}
 
+function timeKiller(event) {
+    event.preventDefault();
+    clearInterval(gameclock);
+    alert('Game cancelled');
+    time = 240;
+}
 
 /* Check if timer is still active AND if wrong answer still = 0 */
 
@@ -22,6 +49,8 @@ var timer = 0;
 
 
 
-
+/* Event handler to cancel quiz */
+cancel.addEventListener('click', timeKiller);
 
 /* Need event handler for quiz start */
+start.addEventListener("click", timer);
